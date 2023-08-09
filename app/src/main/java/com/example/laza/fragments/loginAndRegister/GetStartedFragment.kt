c86146c9246686f06359210faf163276c5f81cc1
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-08-10 01:16:53 +0300

    Implement Animations

--- app/src/main/java/com/example/laza/fragments/loginAndRegister/GetStartedFragment.kt
+++ app/src/main/java/com/example/laza/fragments/loginAndRegister/GetStartedFragment.kt
@@ -1,16 +1,22 @@
 package com.example.laza.fragments.loginAndRegister
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.example.laza.R
 import com.example.laza.databinding.FragmentGetStartedBinding
+import com.google.android.material.snackbar.Snackbar
 
 class GetStartedFragment : Fragment() {
     private lateinit var binding: FragmentGetStartedBinding
+    private var doubleBackToExitPressedOnce = false
+    private val doublePressHandler = Handler()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,8 +35,51 @@ class GetStartedFragment : Fragment() {
         binding.logIn.setOnClickListener {
             findNavController().navigate(R.id.action_getStartedFragment_to_loginFragment)
         }
+
         binding.arrow1.setOnClickListener {
-            findNavController().navigateUp()
+            if (doubleBackToExitPressedOnce){
+                requireActivity().finish()
+            } else {
+                doubleBackToExitPressedOnce = true
+                showExitSnackBar()
+
+                doublePressHandler.postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, 2000) // Delay for resetting the double press flag
+            }
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback {
+            if (doubleBackToExitPressedOnce){
+                requireActivity().finish()
+            } else {
+                doubleBackToExitPressedOnce = true
+                showExitSnackBar()
+
+                doublePressHandler.postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, 2000) // Delay for resetting the double press flag
+            }
+        }
+    }
+
+    private fun showExitSnackBar() {
+        Snackbar.make(binding.root, "Press again to exit the app", Snackbar.LENGTH_SHORT)
+            .show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        doublePressHandler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        doubleBackToExitPressedOnce = false // Reset the flag when the fragment is resumed
+    }
+
+    override fun onPause() {
+        super.onPause()
+        doublePressHandler.removeCallbacksAndMessages(null) // Remove callbacks when the fragment is paused
     }
 }
