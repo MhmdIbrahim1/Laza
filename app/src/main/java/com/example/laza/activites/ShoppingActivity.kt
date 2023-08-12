4e7dd29f18eb562b2717f2eb2c3958eec14c98a2
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-08-12 22:45:26 +0300

    add navigation drawer

--- app/src/main/java/com/example/laza/activites/ShoppingActivity.kt
+++ app/src/main/java/com/example/laza/activites/ShoppingActivity.kt
@@ -2,22 +2,33 @@ package com.example.laza.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.laza.R
 import com.example.laza.databinding.ActivityShoppingBinding
-import com.google.android.material.bottomnavigation.LabelVisibilityMode
-import com.google.android.material.navigation.NavigationBarView
+import com.example.laza.fragments.shopping.HomeFragment
+import de.hdodenhof.circleimageview.CircleImageView
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), HomeFragment.DrawerOpener {
     private val binding by lazy { ActivityShoppingBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
         // set up the navigation controller
         val navController = findNavController(R.id.shoppingHostFragment)
         binding.bottomNavigation.setupWithNavController(navController)
+
+    }
+
+    // Implementation of the DrawerOpener interface
+    override fun openDrawer() {
+        // Implement the logic to open the drawer here
+        val drawerLayout = binding.drawerLayout
+        drawerLayout.openDrawer(GravityCompat.START)
     }
 }
--- app/src/main/java/com/example/laza/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/laza/fragments/shopping/HomeFragment.kt
@@ -1,7 +1,43 @@
 package com.example.laza.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.laza.R
+import com.example.laza.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home){
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var drawerOpener: DrawerOpener
+
+    // Interface to communicate with the activity
+    interface DrawerOpener {
+        fun openDrawer()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Check if the host activity implements the DrawerOpener interface
+        if (activity is DrawerOpener) {
+            drawerOpener = activity as DrawerOpener
+        } else {
+            throw IllegalArgumentException("Host activity must implement DrawerOpener interface")
+        }
+
+        binding.circleImageMenu.setOnClickListener {
+            // Open the drawer by calling the interface method
+            drawerOpener.openDrawer()
+        }
+    }
 }
