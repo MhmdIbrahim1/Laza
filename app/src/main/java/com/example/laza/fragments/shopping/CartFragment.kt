13298a65257b350201db0c9d6b30b030020fea26
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-08-27 04:13:23 +0300

    design the cart fragment_cart.xml design the cart_product_item.xml

--- app/src/main/java/com/example/laza/fragments/shopping/CartFragment.kt
+++ app/src/main/java/com/example/laza/fragments/shopping/CartFragment.kt
@@ -1,7 +1,25 @@
 package com.example.laza.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.laza.R
+import com.example.laza.databinding.FragmentCartBinding
 
-class CartFragment: Fragment(R.layout.fragment_cart){
+class CartFragment: Fragment(){
+    private lateinit var binding: FragmentCartBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
